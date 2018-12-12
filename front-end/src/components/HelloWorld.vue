eae21b036ca727522933768e0ae0f9654f07facb
Riyaz Khan <i7.co.in@gmail.com>
2018-12-12 16:57:24 +0400

    pulled changes

--- front-end/src/components/HelloWorld.vue
+++ front-end/src/components/HelloWorld.vue
@@ -1,40 +1,93 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <el-col :span="10">
+      <el-card>
+        <el-form>
+          <el-form-item label="Pet Type">
+            <el-input v-model="petType"></el-input>
+            {{petType}}
+          </el-form-item>
+          <el-form-item label="Pet Size">
+            <el-input v-model="petSize"></el-input>
+          </el-form-item>
+          <el-form-item label="Pet Color">
+            <el-input v-model="petColor"></el-input>
+          </el-form-item>
+          <el-form-item label="Availability">
+            <el-input v-model="petAvailability"></el-input>
+          </el-form-item>
+          <el-button type="primary" @click="savePet()">Save</el-button>
+        </el-form>
+      </el-card>
+    </el-col>
+    <el-col :span="10" :offset="4">
+      <el-card>
+        <div v-for="(pet, key) in allPets" :key="key">
+          <h3>{{pet.petType}}</h3>
+          <el-button type="primary" @click="showPet(pet._id)">View It</el-button>
+          <el-button type="danger" @click="deletePet(pet._id)">Delete</el-button>
+        </div>
+      </el-card>
+    </el-col>
+
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      albums: null,
+      allPets: null,
+      petType: null,
+      petColor: null,
+      onePet: {},
+      petSize: null,
+      petAvailability: null
+    }
+  },
+  mounted () {
+    this.getPets()
+  },
+  methods: {
+    getPets () {
+      axios.get('http://localhost:8081/pet').then((res) => {
+        this.allPets = res.data.pets
+      })
+    },
+    showPet (value) {
+      axios.get('http://localhost:8081/pet/' + value).then((res) => {
+        this.onePet = res.data.pet
+        this.getPets()
+      })
+    },
+    deletePet (value) {
+      axios.get('http://localhost:8081/pet/delete/' + value).then((result) => {
+        console.log(result)
+        this.getPets()
+      },
+      (err) => {
+        console.log(err, 'there is error')
+      })
+    },
+    savePet () {
+      axios.post('http://localhost:8081/pet', {
+        petType: this.petType,
+        availability: this.petAvailability,
+        color: this.petColor,
+        size: this.petSize
+      }).then((response) => {
+        console.log(response)
+        this.getPets()
+      }, (err) => {
+        console.log(err)
+      })
+    }
   }
 }
 </script>
